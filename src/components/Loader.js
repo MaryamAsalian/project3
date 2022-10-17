d135f22fbf1d0c110e74831a4d53797c16d4b3d2
maryam <maryamasaliyan@yahoo.com.com>
2022-10-17 14:55:26 +0330

    commit 1 on master

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loadin from '../gif/loading.gif'
+
+const Loader = () => {
+    return (
+        <div>
+            <img src ={Loadin} alt = "ssss" />
+            <h1>loading...</h1>
+        </div>
+    );
+};
+
+export default Loader;
